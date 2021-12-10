23da7d955ba1945ac633cf2795f5dccee6a13a95
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-12-10 11:57:02 +0300

    red marker in progress (mark in local files)

--- client/src/main/java/ru/gb/storage/client/io/LocalFileManager.java
+++ client/src/main/java/ru/gb/storage/client/io/LocalFileManager.java
@@ -2,8 +2,8 @@ package ru.gb.storage.client.io;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import lombok.Getter;
 import ru.gb.storage.commons.io.File;
-
 
 import java.awt.*;
 import java.io.IOException;
@@ -11,13 +11,17 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LocalFileManager {
 
     public static String FS_SEPARATOR = FileSystems.getDefault().getSeparator();
+    @Getter
+    private final Map<Path, List<File>> prevStateDir = new HashMap<>();
 
     public ObservableList<File> getLocalFiles(String path) {
         final ObservableList<File> files = FXCollections.observableArrayList();
@@ -34,8 +38,28 @@ public class LocalFileManager {
                             null,
                             null
                     ))
+                    .filter(file -> {
+                        if (file.getIsDirectory()) {
+                            return true;
+                        }
+                        final List<File> prevFiles = prevStateDir.get(Path.of(path));
+                        if (prevFiles != null) {
+                            if (!prevFiles.contains(file)) {
+                                file.setIsReady(false);
+                            } else {
+                                prevFiles.stream()
+                                        .filter(f -> f.equals(file))
+                                        .findFirst()
+                                        .ifPresent(prevFile -> {
+                                            file.setIsReady(prevFile.getIsReady());
+                                        });
+                            }
+                        }
+                        return true;
+                    })
                     .collect(Collectors.toList());
             files.addAll(fileList);
+            prevStateDir.put(Path.of(path), fileList);
         } catch (IOException e) {
             e.printStackTrace();
         }
